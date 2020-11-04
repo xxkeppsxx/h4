ec2024c3b3d55c5311e29b4438ad95185ef19962
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-11-04 13:23:36 +0100

    isint ()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,20 @@
 package com.company;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
 
 public class Main {
-    static List<String[]> list = new ArrayList<>();
+    static List<Integer> list = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-
 
 
         System.out.println("Teile eingeben: ");
@@ -22,20 +22,57 @@ public class Main {
         System.out.println("Teiler eingeben");
         int teiler = Integer.parseInt(sc.nextLine());
 
+        Main m = new Main();
+        m.readfile();
+        m.print(teile,teiler);
 
-        }
-        public void readfile(){
-            File file = new File("numbers.csv");
-            try {
-                list = Files.lines(file.toPath())
-                        .skip(1)
-                        .map(s -> s.split(":"))
+    }
 
-                        .collect(Collectors.toList());
-            } catch (IOException e) {
-                e.printStackTrace();
+    public void readfile() {
+
+        File file = new File("numbers.csv");
+        try (BufferedReader br = new BufferedReader(new FileReader(file));) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] arr = line.split(":");
+                for (int i = 0; i < arr.length; i++) {
+
+                    if(isint(arr[i])){
+                        list.add(Integer.parseInt(arr[i]));
+                    }
+
+                }
             }
-            System.out.println(list);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean isint(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+
+        } catch (NumberFormatException n) {
+            return false;
+        }
+    }
+
+    public void print(int teile,int teiler){
+        ThreadPoolExecutor tpe = (ThreadPoolExecutor) Executors.newFixedThreadPool(teile);
+        List<Integer> list1 = null;
+        int numberofpartstodivide = list.size()/teile;
+        for (int i = 0; i < teile; i++) {
+            list1 = list.subList(i * numberofpartstodivide,i *numberofpartstodivide + numberofpartstodivide);
+            MyThread thread = new MyThread(list1,teiler);
+            tpe.execute(thread);
+
 
         }
-}
+        tpe.shutdown();
+        }
+    }
--- src/com/company/MyThread.java
+++ src/com/company/MyThread.java
@@ -1,7 +1,20 @@
 package com.company;
 
-public class MyThread extends Thread{
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyThread implements Runnable{
+    List <Integer> list ;
+    int teiler;
+
+    public MyThread(List<Integer> list, int teiler){
+        this.list = list;
+        this.teiler = teiler;
+    }
 public void run(){
-    for(int i = 0 ; i<Main.list.size()/Main.;)
+    list = list.stream().filter(i -> i%teiler == 0).collect(Collectors.toList());
+    for (Integer i:list) {
+        System.out.println(i);
+    }
 }
 }

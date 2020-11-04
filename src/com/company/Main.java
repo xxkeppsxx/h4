5b5dac9901d68b838be5904990126bfe7bfbce59
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-11-04 12:35:53 +0100

    readfile class created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
+    static List<String[]> list = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+
+
+        System.out.println("Teile eingeben: ");
+        int teile = Integer.parseInt(sc.nextLine());
+        System.out.println("Teiler eingeben");
+        int teiler = Integer.parseInt(sc.nextLine());
+
+
+        }
+        public void readfile(){
+            File file = new File("numbers.csv");
+            try {
+                list = Files.lines(file.toPath())
+                        .skip(1)
+                        .map(s -> s.split(":"))
+
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(list);
+
+        }
 }
---
+++ src/com/company/MyThread.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class MyThread extends Thread{
+public void run(){
+    for(int i = 0 ; i<Main.list.size()/Main.;)
+}
+}

ec97a66a8983556cb84c4bea40ab9c0252df9206
Lukas Kepplinger <lukas.kepplinger@gmx.net>
2020-11-06 13:03:12 +0100

    MyThreadsum class

---
+++ src/com/company/MyThreadsum.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class MyThreadsum extends Thread {
+    
+}
---
+++ src/com/company/Summenformel.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Summenformel {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Obergrenze : ");
+        int obergrenze = Integer.parseInt(sc.nextLine());
+
+    }
+
+    public void sumcalc(int obergrenze){
+
+    }
+}
